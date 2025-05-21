5c44e485487761691a6dba0a8fdb28c5355bce8a
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-05-22 03:30:25 +0530

    removed unnecessary things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-'use client'
 
-import Button from "@/components/Button";
-import { Hero } from "@/components/Hero";
+
+
 import Navbar from "@/components/Navbar";
-import { toast } from "sonner";
+
 
 console.log(process.env.GOOGLE_CLIENT_ID);
 console.log(process.env.GOOGLE_CLIENT_SECRET);
@@ -15,10 +14,10 @@ export default function Home() {
     <div className="">
       <Navbar />
       <div className="mt-24">
-        <Hero/>
-        <Button variant="primary" onClick={() => {
+        {/* <Hero/> */}
+        {/* <Button variant="primary" onClick={() => {
           toast("Hello sir !!")
-        }}>Hello</Button>
+        }}>Hello</Button> */}
       </div>
     </div>
   );
