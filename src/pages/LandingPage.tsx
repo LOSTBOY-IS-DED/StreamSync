b04a17b587d1142ed1d3d806e73f187587039902
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-05-23 18:17:36 +0530

    pushing again

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,23 +1,22 @@
 "use client";
-export const dynamic = 'force-dynamic';
 
-import Navbar from "@/components/Navbar";
+import dynamicImport from "next/dynamic";
 import Hero from "@/sections/Hero";
 import Introduction from "@/sections/Introduction";
 import Features3D from "@/components/features-3d";
-import { Redirect } from "@/components/Redirect";
 
+// Marking dynamic imports with ssr: false
+const Navbar = dynamicImport(() => import("@/components/Navbar"), { ssr: false });
+const Redirect = dynamicImport(() => import("@/components/Redirect").then(mod => mod.Redirect), { ssr: false });
 
-export default function LandingPage(){
-    return(
-        <>
-        <main className="min-h-screen bg-black text-white pt-20">
+export default function LandingPage() {
+  return (
+    <main className="min-h-screen bg-black text-white pt-20">
       <Navbar />
       <Redirect />
       <Hero />
       <Introduction />
       <Features3D />
     </main>
-        </>
-    )
+  );
 }
